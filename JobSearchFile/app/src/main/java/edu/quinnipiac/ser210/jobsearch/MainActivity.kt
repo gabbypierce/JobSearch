71c04158d39ec68f607900c081381fc2f3e766e8
SamWoodburn25 <samantha.woodburn@quinnipiac.edu>
2024-03-20 14:52:53 -0400

    update

--- JobSearchFile/app/src/main/java/edu/quinnipiac/ser210/jobsearch/MainActivity.kt
+++ JobSearchFile/app/src/main/java/edu/quinnipiac/ser210/jobsearch/MainActivity.kt
@@ -2,12 +2,30 @@ package edu.quinnipiac.ser210.jobsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.materialToolbar))
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val config = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController!!, config)
+
+        findViewById<NavigationView>(R.id.navigationView).setupWithNavController(navController!!)
 
     }
 }
